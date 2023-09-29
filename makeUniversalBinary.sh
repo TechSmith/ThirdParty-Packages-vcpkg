10ba135a79a4afe3696c02465a3de431f6ca5756
Mike Malburg <m.malburg@techsmith.com>
2023-09-29 12:12:57 -0400

    Suggestion: Combine universal library logic for static and dynamic libs

--- makeUniversalBinary.sh
+++ makeUniversalBinary.sh
@@ -1,56 +1,55 @@
 if [ $# -lt 3 ]; then
-    echo "Usage: $0 <src_x64_path> <src_arm64_path> <dest_universal_path>"
+    echo "Usage: $0 <src_x64_dir> <src_arm64_dir> <dest_universal_dir>"
     exit 1
 fi
 
-src_x64_path=$1
-src_arm64_path=$2
-dest_universal_path=$3
+src_x64_dir=$1
+src_arm64_dir=$2
+dest_universal_dir=$3
 
-X86_64_LIB=${src_x64_path}/lib
-ARM64_LIB=${src_arm64_path}/lib
-UNIVERSAL_LIB=${dest_universal_path}/lib
+X86_64_LIB_DIR=${src_x64_dir}/lib
+ARM64_LIB_DIR=${src_arm64_dir}/lib
+UNIVERSAL_LIB_DIR=${dest_universal_dir}/lib
 
-rm -rf ${dest_universal_path} # start clean
+rm -rf ${dest_universal_dir} # start clean
 
-mkdir -p ${UNIVERSAL_LIB}
+mkdir -p ${UNIVERSAL_LIB_DIR}
 
 # Assume arm64 and x86_64 are identical.
-cp -R ${src_arm64_path}/include ${dest_universal_path}/include
+cp -R ${src_arm64_dir}/include ${dest_universal_dir}/include
 
 # Make install paths relative for both arm64 and x86_64
 #
 # Only do this on actual files, not symbolic links (-type f)
-find ${ARM64_LIB} -name "*.a" -type f -exec chmod +w {} \;
-find ${ARM64_LIB} -name "*.a" -type f -exec python3 makeInstallPathsRelative.py @rpath {} \;
-find ${X86_64_LIB} -name "*.a" -type f -exec chmod +w {} \;
-find ${X86_64_LIB} -name "*.a" -type f -exec python3 makeInstallPathsRelative.py @rpath {} \;
 
-# look through arm64 dylibs and start lipo'ing
-for staticLibPath in ${ARM64_LIB}/*.a; do   
-   filename="$(basename $staticLibPath)"
-   if [[ -L "$staticLibPath" ]]; then
-     echo "lib = $staticLibPath - copy symlink"
-     cp -a $staticLibPath ${UNIVERSAL_LIB}/$filename
-   else
-     echo "lib = $staticLibPath - need lipo"
-     lipo -create -output ${UNIVERSAL_LIB}/$filename ${ARM64_LIB}/$filename ${X86_64_LIB}/$filename
-   fi
-done
+# Function to make install paths relative
+makeInstallPathsRelative() {
+  find "$1" -name "*.$2" -type f -exec chmod +w {} \;
+  find "$1" -name "*.$2" -type f -exec python3 makeInstallPathsRelative.py @rpath {} \;
+}
 
-find ${ARM64_LIB} -name "*.dylib" -type f -exec chmod +w {} \;
-find ${ARM64_LIB} -name "*.dylib" -type f -exec python3 makeInstallPathsRelative.py @rpath {} \;
-find ${X86_64_LIB} -name "*.dylib" -type f -exec chmod +w {} \;
-find ${X86_64_LIB} -name "*.dylib" -type f -exec python3 makeInstallPathsRelative.py @rpath {} \;
+# Function to create universal binaries for .a and .dylib files
+createUniversalBinaries() {
+  local source_dir="$1"
+  local destination_dir="$2"
 
-# look through arm64 dylibs and start lipo'ing
-for dyLibPath in ${ARM64_LIB}/*.dylib; do   
-   filename="$(basename $dyLibPath)"
-   if [[ -L "$dyLibPath" ]]; then
-     echo "lib = $dyLibPath - copy symlink"
-     cp -a $dyLibPath ${UNIVERSAL_LIB}/$filename
-   else
-     echo "lib = $dyLibPath - need lipo"
-     lipo -create -output ${UNIVERSAL_LIB}/$filename ${ARM64_LIB}/$filename ${X86_64_LIB}/$filename
-   fi
-done
+  for libPath in "${source_dir}"/*.{a,dylib}; do
+    filename="$(basename "$libPath")"
+    if [[ -L "$libPath" ]]; then
+      echo "lib = $libPath - copy symlink"
+      cp -a "$libPath" "${destination_dir}/$filename"
+    else
+      echo "lib = $libPath - need lipo"
+      lipo -create -output "${destination_dir}/$filename" "${ARM64_LIB_DIR}/$filename" "${X86_64_LIB_DIR}/$filename"
+    fi
+  done
+}
+
+# Make install paths relative for both .a and .dylib files
+makeInstallPathsRelative "$ARM64_LIB_DIR" "a"
+makeInstallPathsRelative "$X86_64_LIB_DIR" "a"
+makeInstallPathsRelative "$ARM64_LIB_DIR" "dylib"
+makeInstallPathsRelative "$X86_64_LIB_DIR" "dylib"
+
+# Create universal binaries for .a and .dylib files
+createUniversalBinaries "$ARM64_LIB_DIR" "$UNIVERSAL_LIB_DIR"
