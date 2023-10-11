6f38518eac0c3dc813c335715c8fb28b2caded58
Mike Malburg <m.malburg@techsmith.com>
2023-10-11 10:37:19 -0400

    Extract build-and-stage.ps1 steps into functions
    This should hopefully make the main portion of the script a bit easier to follow.

--- build-and-stage.ps1
+++ build-and-stage.ps1
@@ -10,194 +10,227 @@ param(
 
 Import-Module "$PSScriptRoot/ps-modules/Util"
 
-Write-Banner -Level 2 -Title "Starting vcpkg install for: $PackageAndFeatures"
-$allParams = @{
-    PackageName = $PackageName
-    PackageAndFeatures = $PackageAndFeatures
-    LinkType = $LinkType
-    BuildType = $BuildType
-    StagedArtifactsPath = $StagedArtifactsPath
-    VcpkgHash = $VcpkgHash
-    ShowDebug = $ShowDebug
-}
-Write-Message "Parameters:"
-foreach ($paramName in $allParams.Keys) {
-    $paramValue = $allParams[$paramName]
-    Write-Message "- $paramName`: $paramValue"
-}
-
-Write-Banner -Level 3 -Title "Initializing"
-$vcpkgRepo = "https://github.com/TechSmith/vcpkg.git"
-$IsOnWindowsOS = Get-IsOnWindowsOS
-$IsOnMacOS = Get-IsOnMacOS
-
-$packageNameOnly = $PackageAndFeatures -replace '\[.*$', ''
-if( $PackageName -eq "") {
-   $PackageName = "$packageNameOnly-$LinkType"
-}
-
-$osName = ""
-$vcpkgExe = ""
-$vcpkgCacheDir = ""
-$vcpkgBootstrapScript = ""
-$preStagePath = ""
-$triplets = @()
-if ($IsOnWindowsOS) {
-   $osName = "Windows"
-   $vcpkgExe = "./vcpkg/vcpkg.exe"
-   $vcpkgCacheDir = "$env:LocalAppData\vcpkg\archives"
-   $vcpkgBootstrapScript = "./bootstrap-vcpkg.bat"
-   $triplets += "x64-windows-$LinkType-$BuildType"
-   $preStagePath = "vcpkg/installed/$($triplets[0])"
-   $artifactSubfolder = "$PackageName-windows-$BuildType"
-} elseif ($IsOnMacOS) {
-   $osName = "Mac"
-   $vcpkgExe = "./vcpkg/vcpkg"
-   $vcpkgCacheDir = "$HOME/.cache/vcpkg/archives"
-   $vcpkgBootstrapScript = "./bootstrap-vcpkg.sh"
-   $triplets += "x64-osx-$LinkType-$BuildType"
-   $triplets += "arm64-osx-$LinkType-$BuildType"
-   $preStagePath = "universal"
-   $artifactSubfolder = "$PackageName-osx-$BuildType"
-}
-$artifactArchive = "$artifactSubfolder.tar.gz"
-
-$initParams = @{
-   vcpkgRepo = $vcpkgRepo
-   IsOnWindowsOS = $IsOnWindowsOS
-   IsOnMacOS = $IsOnMacOS
-   packageNameOnly = $packageNameOnly
-   osName = $osName
-   vcpkgExe = $vcpkgExe
-   vcpkgCacheDir = $vcpkgCacheDir
-   vcpkgBootstrapScript = $vcpkgBootstrapScript
-   triplets = $triplets
-   preStagePath = $preStagePath
-   artifactSubfolder = $artifactSubfolder
-}
-
-Write-Message "Initialized vars:"
-foreach ($paramName in $initParams.Keys) {
-    $paramValue = $initParams[$paramName]
-    Write-Message "- $paramName`: $paramValue"
-}
-
-Write-Banner -Level 3 -Title "Setting up vcpkg"
-Write-Message "Removing vcpkg system cache..."
-Write-Message "> Looking for user-specific vcpkg cache dir: $vcpkgCacheDir"
-if (Test-Path -Path $vcpkgCacheDir -PathType Container) {
-   Write-Message "> Deleting dir: $vcpkgCacheDir"
-   Remove-Item -Path $vcpkgCacheDir -Recurse -Force
-} else {
-   Write-Message "> Directory not found: $vcpkgCacheDir"
-}    
-
-Write-Message "$(NL)Removing dir: vcpkg"
-$vcpkgDir = "./vcpkg"
-if (Test-Path -Path $vcpkgDir -PathType Container) {
-   Write-Message "> Directory found. Deleting: $vcpkgDir"
-   Remove-Item -Path $vcpkgDir -Recurse -Force
-} else {
-   Write-Message "> Directory not found: $vcpkgDir, skipping step."
-}
-
-Write-Message "$(NL)Installing vcpkg..."
-if (Test-Path -Path $vcpkgDir -PathType Container) {
-    Write-Message "> Directory already exists: $vcpkgDir!!!"
-}
-else
-{
-    Write-Message "> Cloning repo: $vcpkgRepo"
-    git clone $vcpkgRepo
-    if ($VcpkgHash -ne "") {
-        Write-Message "> Using hash: $VcpkgHash"
-        Push-Location vcpkg
-        git checkout $VcpkgHash
-        Pop-Location
+function Write-Debug { 
+    param([string]$Message)
+    if($ShowDebug) {
+        Write-Message $Message
     }
-
-    Write-Message "$(NL)Bootstrapping vcpkg..."
-    Push-Location "vcpkg"
-    Invoke-Expression "$vcpkgBootstrapScript"
-    Pop-Location
 }
 
-Write-Banner -Level 3 -Title "Pre-build step"
-$preBuildScript = "custom-steps/$packageNameOnly/pre-build.ps1"
-if (Test-Path -Path $preBuildScript -PathType Leaf) {
-   Invoke-Powershell -FilePath $preBuildScript
-} else {
-   Write-Message "File does not exist: $preBuildScript.  Skipping step..."
-}
-[Console]::Out.Flush()
+function Initialize-Variables {
+   $vcpkgRepo = "https://github.com/TechSmith/vcpkg.git"
+   $isOnWindowsOS = Get-isOnWindowsOS
+   $isOnMacOS = Get-isOnMacOS
+   $packageNameOnly = $PackageAndFeatures -replace '\[.*$', ''
+   if( $PackageName -eq "") {
+      $PackageName = "$packageNameOnly-$LinkType"
+   }
+   $triplets = @()
+   if ($isOnWindowsOS) {
+       $osName = "Windows"
+       $vcpkgExe = "./vcpkg/vcpkg.exe"
+       $vcpkgCacheDir = "$env:LocalAppData\vcpkg\archives"
+       $vcpkgBootstrapScript = "./bootstrap-vcpkg.bat"
+       $triplets += "x64-windows-$LinkType-$BuildType"
+       $preStagePath = "vcpkg/installed/$($triplets[0])"
+       $artifactName = "$PackageName-windows-$BuildType"
+   } elseif ($isOnMacOS) {
+       $osName = "Mac"
+       $vcpkgExe = "./vcpkg/vcpkg"
+       $vcpkgCacheDir = "$HOME/.cache/vcpkg/archives"
+       $vcpkgBootstrapScript = "./bootstrap-vcpkg.sh"
+       $triplets += "x64-osx-$LinkType-$BuildType"
+       $triplets += "arm64-osx-$LinkType-$BuildType"
+       $preStagePath = "universal"
+       $artifactName = "$PackageName-osx-$BuildType"
+   }
 
-Write-Banner -Level 3 -Title "Installing package: $PackageAndFeatures"
-$tripletCount = $triplets.Length
-$tripletNum = 1
-foreach ($triplet in $triplets) {
-   Write-Message "Installing for triplet $tripletNum/$tripletCount`: $triplet..."
-   Install-FromVcPkg -Package $PackageAndFeatures -Triplet $triplet -vcpkgExe $vcpkgExe
-   Exit-IfError $LASTEXITCODE
-   $tripletNum++
+   $vars = @{
+       vcpkgRepo = $vcpkgRepo
+       vcpkgRepoHash = $VcpkgHash
+       vcpkgInstallDir = "./vcpkg"
+       vcpkgExe = $vcpkgExe
+       vcpkgCacheDir = $vcpkgCacheDir
+       vcpkgBootstrapScript = $vcpkgBootstrapScript
+       isOnWindowsOS = $isOnWindowsOS
+       isOnMacOS = $isOnMacOS
+       packageNameOnly = $packageNameOnly
+       osName = $osName
+       triplets = $triplets
+       preStagePath = $preStagePath
+       stagePath = $StagedArtifactsPath
+       artifactName = $artifactName
+       prebuildScript = "custom-steps/$packageNameOnly/pre-build.ps1"
+       postbuildScript = "custom-steps/$packageNameOnly/post-build.ps1"
+   }
+   Write-Debug "$(NL)Initialized vars:"
+   Write-Debug (Get-PSObjectAsFormattedList -Object $vars)
+   return $vars
 }
 
-if($IsOnMacOS) {
+function Setup-Vcpkg {
+   param(
+      [string]$repo,
+      [string]$repoHash,
+      [string]$installDir,
+      [string]$cacheDir,
+      [string]$bootstrapScript
+   )
+   Write-Message "Removing vcpkg..."
+   Write-Debug "> Removing vcpkg system cache..."
+   Write-Debug ">> Looking for user-specific vcpkg cache dir: $cacheDir"
+   if (Test-Path -Path $cacheDir -PathType Container) {
+      Write-Debug ">> Deleting dir: $cacheDir"
+      Remove-Item -Path $cacheDir -Recurse -Force
+   } else {
+      Write-Debug ">> Directory not found: $cacheDir"
+   }    
+   Write-Debug "> Removing dir: vcpkg"
+   if (Test-Path -Path $installDir -PathType Container) {
+      Write-Debug ">> Directory found. Deleting: $installDir"
+      Remove-Item -Path $installDir -Recurse -Force
+   } else {
+      Write-Debug ">> Directory not found: $installDir, skipping step."
+   }
+
+   Write-Message "$(NL)Installing vcpkg..."
+   if (Test-Path -Path $installDir -PathType Container) {
+       Write-Debug "> Directory already exists: $installDir!!!"
+   }
+   else
+   {
+       Write-Debug "> Cloning repo: $repo"
+       git clone $repo
+       if ($repoHash -ne "") {
+           Write-Debug ">> Checking out hash: $repoHash"
+           Push-Location vcpkg
+           git checkout $repoHash
+           Pop-Location
+       }
+
+       Write-Debug "> Bootstrapping vcpkg..."
+       Push-Location $installDir
+       Invoke-Expression "$bootstrapScript"
+       Pop-Location
+   }
+}
+
+function Run-ScriptIfExists {
+   param(
+      [string]$title,
+      [string]$script,
+      [PSObject]$scriptArgs
+   )
+   if ( -not (Test-Path -Path $script -PathType Leaf) ) {
+      return
+   }
+   Write-Banner -Level 3 -Title $title
+   Write-Debug "> Executing: $script"
+   Invoke-Powershell -FilePath $script -ArgumentList $scriptArgs
+}
+
+function Run-PreBuildScriptIfExists {
+   param(
+      $script
+   )
+   Run-ScriptIfExists -title "Pre-build step" -script $script
+}
+
+function Run-PostBuildScriptIfExists {
+   param(
+      $script,
+      $preStagePath
+   )
+   $scriptArgs = @{ BuildArtifactsPath = ((Resolve-Path $preStagePath).Path -replace '\\', '/') }
+   Run-ScriptIfExists -title "Post-build step" -script $script -scriptArgs $scriptArgs
+}
+
+function Install-Package
+{
+   param(
+      [string]$vcpkgExe,
+      [string]$package,
+      [PSObject]$triplets
+   )
+   $tripletCount = $triplets.Length
+   Write-Message "Installing for $tripletCount triplet(s)..."
+
+   $tripletNum = 1
+   foreach ($triplet in $triplets) {
+      Write-Message "> Installing for triplet $tripletNum/$tripletCount`: $triplet..."
+      Install-FromVcPkg -Package $package -Triplet $triplet -vcpkgExe $vcpkgExe
+      Exit-IfError $LASTEXITCODE
+      $tripletNum++
+   }
+}
+
+function Create-UniversalBinaryIfOnMac {
+   param(
+      $vcpkgInstallDir
+   )
+   if (-not (Get-isOnMacOS)) { return }
    Write-Banner -Level 3 -Title "Creating universal binary"
-   $arm64Dir = "vcpkg/installed/$($triplets[0])"
-   $x64Dir = "vcpkg/installed/$($triplets[1])"
-   Write-Message "$arm64Dir, $x64Dir ==> $preStagePath"
+   $arm64Dir = "$vcpkgInstallDir/installed/$($triplets[0])"
+   $x64Dir = "$vcpkgInstallDir/installed/$($triplets[1])"
+   Write-Debug "$arm64Dir, $x64Dir ==> $preStagePath"
    ConvertTo-UniversalBinaries -arm64Dir "$arm64Dir" -x64Dir "$x64Dir" -universalDir "$preStagePath"
 }
 
-Write-Banner -Level 3 -Title "Post-build step"
-$postBuildScript = "custom-steps/$packageNameOnly/post-build.ps1"
-$preStagePath = (Resolve-Path $preStagePath).Path -replace '\\', '/' # Expand this, so it is an absolute path we are passing to other scripts
-if (Test-Path -Path $postBuildScript -PathType Leaf) {
-    $scriptArgs = @{ "BuildArtifactsPath" = "$preStagePath" }
-    Invoke-Powershell -FilePath $postBuildScript -ArgumentList $scriptArgs
-} else {
-    Write-Message "File does not exist: $postBuildScript.  Skipping step..."
+function Stage-Artifacts {
+   param(
+      $vcpkgExe,
+      $preStagePath,
+      $stagePath,
+      $artifactName
+   )
+
+   Write-Debug "Creating dir: $artifactName"
+   New-Item -Path $stagePath/$artifactName -ItemType Directory -Force -ErrorAction SilentlyContinue | Out-Null
+
+   $dependenciesFilename = "dependencies.json"
+   Write-Debug "Generating: `"$dependenciesFilename`"..."
+   Write-Debug "> $stagePath/$artifactName/$dependenciesFilename"
+   Invoke-Expression "$vcpkgExe list --x-json > $stagePath/$artifactName/$dependenciesFilename"
+
+   $packageInfoFilename = "package.json"
+   Write-Debug "Generating: `"$packageInfoFilename`"..."
+   $dependenciesJson = Get-Content -Raw -Path "$stagePath/$artifactName/$dependenciesFilename" | ConvertFrom-Json
+   $packageVersion = ($dependenciesJson.PSObject.Properties.Value | Where-Object { $_.package_name -eq $packageNameOnly } | Select-Object -First 1).version
+   Write-ReleaseInfoJson -PackageName $PackageName -Version $packageVersion -PathToJsonFile "$stagePath/$artifactName/$packageInfoFilename"
+   
+   # TODO: Add info in this file on where each package was downloaded from
+   # TODO: Add license file info to the staged artifacts (ex. per-library LICENSE, COPYING, or other such files that commonly have license info in them)
+   
+   Write-Message "Copying: $preStagePath =`> $artifactName"
+   $excludedFolders = @("tools", "share", "debug")
+   Copy-Item -Path "$preStagePath/*" -Destination $stagePath/$artifactName -Force -Recurse -Exclude $excludedFolders
+   
+   $artifactArchive = "$artifactName.tar.gz"
+   Write-Message "Compressing: `"$artifactName`" =`> `"$artifactArchive`""
+   tar -czf "$stagePath/$artifactArchive" -C "$stagePath/$artifactName" .
+   
+   Write-Debug "Deleting: `"$artifactName`""
+   Remove-Item -Path "$stagePath/$artifactName" -Recurse -Force
 }
+
+Write-Banner -Level 2 -Title "Starting vcpkg install for: $PackageAndFeatures"
+Write-Message "Params:"
+Write-Message (Get-PSObjectAsFormattedList -Object $PSBoundParameters)
+$vars = Initialize-Variables
+
+Write-Banner -Level 3 -Title "Setting up vcpkg"
+Setup-VcPkg -repo $vars.vcpkgRepo -repoHash $vars.vcpkgRepoHash -installDir $vars.vcpkgInstallDir -cacheDir $vars.vcpkgCacheDir -bootstrapScript $vars.vcpkgBootstrapScript
+
+Run-PreBuildScriptIfExists -script $vars.prebuildScript
+
+Write-Banner -Level 3 -Title "Installing package: $PackageAndFeatures"
+Install-Package -vcpkgExe $vars.vcpkgExe -package $PackageAndFeatures -triplets $vars.triplets
+
+Create-UniversalBinaryIfOnMac
+
+Run-PostBuildScriptIfExists -script $vars.postbuildScript -preStagePath $vars.preStagePath
 
 Write-Banner -Level 3 -Title "Stage build artifacts"
-Write-Message "Creating dir: $artifactSubfolder"
-New-Item -Path $StagedArtifactsPath/$artifactSubfolder -ItemType Directory -Force -ErrorAction SilentlyContinue | Out-Null
+Stage-Artifacts -vcPkgExe $vars.vcpkgExe -preStagePath $vars.preStagePath -stagePath $vars.stagePath -artifactName $vars.artifactName
 
-$dependenciesFilename = "dependencies.json"
-Write-Message "$(NL)Generating: `"$dependenciesFilename`"..."
-Invoke-Expression "$vcpkgExe list --x-json > $StagedArtifactsPath/$artifactSubfolder/$dependenciesFilename"
-
-$packageInfoFilename = "package.json"
-Write-Message "$(NL)Generating: `"$packageInfoFilename`"..."
-$dependenciesJson = Get-Content -Raw -Path "$StagedArtifactsPath/$artifactSubfolder/$dependenciesFilename" | ConvertFrom-Json
-$packageVersion = ($dependenciesJson.PSObject.Properties.Value | Where-Object { $_.package_name -eq $packageNameOnly } | Select-Object -First 1).version
-Write-ReleaseInfoJson -PackageName $PackageName -Version $packageVersion -PathToJsonFile "$StagedArtifactsPath/$artifactSubfolder/$packageInfoFilename"
-
-# TODO: Add info in this file on where each package was downloaded from
-# TODO: Add license file info to the staged artifacts (ex. per-library LICENSE, COPYING, or other such files that commonly have license info in them)
-
-Write-Message "$(NL)Copying: $preStagePath =`> $artifactSubfolder"
-$excludedFolders = @("tools", "share", "debug")
-Copy-Item -Path "$preStagePath/*" -Destination $StagedArtifactsPath/$artifactSubfolder -Force -Recurse -Exclude $excludedFolders
-
-Write-Message "$(NL)Compressing: `"$artifactSubfolder`" =`> `"$artifactArchive`""
-tar -czf "$StagedArtifactsPath/$artifactArchive" -C "$StagedArtifactsPath/$artifactSubfolder" .
-
-Write-Message "$(NL)Deleting: `"$artifactSubfolder`""
-Remove-Item -Path "$StagedArtifactsPath/$artifactSubfolder" -Recurse -Force
-
-if($ShowDebug)
-{
-    Write-Banner -Level 3 -Title "Showing debug info"
-    if($IsOnWindowsOS) {
-        # Show Windows debugging info
-        Write-Message "No additional debugging information is available."
-    }
-    else {
-        # Show Mac debugging info
-        Write-Message "No additional debugging information is available."
-    }
-}
-
-# Done
 Write-Message "$(NL)$(NL)Done."
