973d6ea2ae608d7931e75cc2f09db5c1202fa23b
Mike Malburg <m.malburg@techsmith.com>
2023-10-11 10:36:14 -0400

    +Optional ShowDebug param to build-and-stage-preconfigured.ps1

--- build-and-stage-preconfigured.ps1
+++ build-and-stage-preconfigured.ps1
@@ -1,6 +1,7 @@
 param(
     [Parameter(Mandatory=$true)][string]$PackageName, # Name of package from preconfigured-packages.json
-    [string]$StagedArtifactsPath = "StagedArtifacts"  # Output path to stage these artifacts to
+    [string]$StagedArtifactsPath = "StagedArtifacts", # Output path to stage these artifacts to
+    [switch]$ShowDebug = $false
 )
 
 Import-Module "$PSScriptRoot/ps-modules/Util" -Force
@@ -9,4 +10,4 @@ Write-Banner -Level 1 -Title "Installing preconfigured package: `"$PackageName`"
 $pkg = Get-PackageInfo -PackageName $PackageName
 
 Write-Message "$(NL)Running build-and-stage.ps1...$(NL)"
-./build-and-stage.ps1 -PackageName $PackageName -PackageAndFeatures $pkg.package -LinkType $pkg.linkType -BuildType $pkg.buildType -StagedArtifactsPath $StagedArtifactsPath -VcpkgHash $pkg.vcpkgHash
+./build-and-stage.ps1 -PackageName $PackageName -PackageAndFeatures $pkg.package -LinkType $pkg.linkType -BuildType $pkg.buildType -StagedArtifactsPath $StagedArtifactsPath -VcpkgHash $pkg.vcpkgHash -ShowDebug:$ShowDebug
