b8a3960363aef4a9d2109da25bf48c8e6e57dec5
Mike Malburg <m.malburg@techsmith.com>
2024-04-12 09:41:12 -0400

    Update build script names
    Shortening the names of these.

--- build-package.ps1
+++ build-package.ps1
@@ -9,5 +9,5 @@ Import-Module "$PSScriptRoot/ps-modules/Build" -Force -DisableNameChecking
 Write-Banner -Level 1 -Title "Installing preconfigured package: `"$PackageName`""
 $pkg = Get-PackageInfo -PackageName $PackageName
 
-Write-Message "$(NL)Running build-and-stage.ps1...$(NL)"
-./build-and-stage.ps1 -PackageName $PackageName -PackageAndFeatures $pkg.package -LinkType $pkg.linkType -BuildType $pkg.buildType -StagedArtifactsPath $StagedArtifactsPath -VcpkgHash $pkg.vcpkgHash -ShowDebug:$ShowDebug
+Write-Message "$(NL)Running invoke-build.ps1...$(NL)"
+./invoke-build.ps1 -PackageName $PackageName -PackageAndFeatures $pkg.package -LinkType $pkg.linkType -BuildType $pkg.buildType -StagedArtifactsPath $StagedArtifactsPath -VcpkgHash $pkg.vcpkgHash -ShowDebug:$ShowDebug
