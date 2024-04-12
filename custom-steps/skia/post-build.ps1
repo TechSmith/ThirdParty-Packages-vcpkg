4870e5749cc8f9aa89461a1163501938766bd673
Mike Malburg <m.malburg@techsmith.com>
2024-04-12 12:29:59 -0400

    Update other post-build steps with same fix.

--- custom-steps/skia/post-build.ps1
+++ custom-steps/skia/post-build.ps1
@@ -2,8 +2,14 @@ param (
     [Parameter(Mandatory=$true)][string]$BuildArtifactsPath,
     [Parameter(Mandatory=$false)][string]$PackageAndFeatures,
     [Parameter(Mandatory=$false)][string]$LinkType,
-    [Parameter(Mandatory=$false)][string]$BuildType
+    [Parameter(Mandatory=$false)][string]$BuildType,
+    [Parameter(Mandatory=$false)][string]$ModulesRoot    
 )
+
+$moduleName = "Build"
+if(-not (Get-Module -Name $moduleName)) {
+    Import-Module "$ModulesRoot/$moduleName" -Force -DisableNameChecking
+}
 
 if ((Get-IsOnMacOS)) {
     Remove-DylibSymlinks -BuildArtifactsPath $BuildArtifactsPath
--- custom-steps/tinyxml/post-build.ps1
+++ custom-steps/tinyxml/post-build.ps1
@@ -3,8 +3,14 @@ param (
     [Parameter(Mandatory=$true)][string]$BuildArtifactsPath,
     [Parameter(Mandatory=$false)][string]$PackageAndFeatures,
     [Parameter(Mandatory=$false)][string]$LinkType,
-    [Parameter(Mandatory=$false)][string]$BuildType
+    [Parameter(Mandatory=$false)][string]$BuildType,
+    [Parameter(Mandatory=$false)][string]$ModulesRoot    
 )
+
+$moduleName = "Build"
+if(-not (Get-Module -Name $moduleName)) {
+    Import-Module "$ModulesRoot/$moduleName" -Force -DisableNameChecking
+}
 
 Write-Message "Test: Running post-build script..."
 if (-not (Get-IsOnMacOS)) {
