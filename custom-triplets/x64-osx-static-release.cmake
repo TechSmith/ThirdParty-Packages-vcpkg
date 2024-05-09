d520b45fc36880e33deca7d160f273d515c9d58f
Mike Malburg <m.malburg@techsmith.com>
2024-05-08 20:43:42 -0400

    Add debug symbols to Mac build
    Also, upgrade to vcpkg_cmake_configure and vcpkg_cmake_install.  Ninja is preferred by default, now, with these.
    
    Move setting debug symbols to custom triplets
    
    Just set this in the Mac release triplets

--- custom-triplets/x64-osx-static-release.cmake
+++ custom-triplets/x64-osx-static-release.cmake
@@ -7,5 +7,5 @@ set(VCPKG_OSX_ARCHITECTURES x86_64)
 set(VCPKG_BUILD_TYPE release)
 
 set(VCPKG_C_FLAGS -mmacosx-version-min=11.0)
-set(VCPKG_CXX_FLAGS -mmacosx-version-min=11.0)
+set(VCPKG_CXX_FLAGS "-mmacosx-version-min=11.0 -g2 -gdwarf-2")
 set(VCPKG_LINKER_FLAGS -mmacosx-version-min=11.0)
