3dc532cbe9731bc1d1c81572c42b6d5bac08fd82
Mike Malburg <m.malburg@techsmith.com>
2024-05-13 20:05:01 -0400

    Make Remove-DylibSymlinks run for all Mac builds
    We always do this, so lets simplify some post-build scripts (and get rid of some) by adding this to the Create-FinalizedMacArtifacts step.

--- ps-modules/MacBuild/MacBuild.psm1
+++ ps-modules/MacBuild/MacBuild.psm1
@@ -62,6 +62,7 @@ function Create-FinalizedMacArtifacts {
     Copy-NonLibraryFiles -srcDir $arm64LibDir -destDir $universalLibDir
     Remove-Item -Force -Recurse -Path $arm64LibDir
     Remove-Item -Force -Recurse -Path $x64LibDir
+    Remove-DylibSymlinks -libDir $universalLibDir
 }
 
 function Update-LibraryPath {
@@ -193,11 +194,11 @@ function Get-FilesAndSymlinks {
 
 function Remove-DylibSymlinks {
     param (
-        [Parameter(Mandatory=$true)][string]$BuildArtifactsPath
+        [Parameter(Mandatory=$true)][string]$libDir
     )
 
     Write-Message "Consolidating libraries and symlinks..."
-    Push-Location "$BuildArtifactsPath/lib"
+    Push-Location "$libDir"
 
     # Enumerate files
     $physicalFiles = @()
@@ -264,3 +265,4 @@ function Remove-DylibSymlinks {
 }
 
 Export-ModuleMember -Function Create-FinalizedMacArtifacts, Remove-DylibSymlinks
+Export-ModuleMember -Function Create-FinalizedMacArtifacts
