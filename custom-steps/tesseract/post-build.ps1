c87b0ca028b991f2e52a9039ae81a5167a9a1926
Matthew Zepf <m.zepf@techsmith.com>
2024-06-20 13:47:35 -0400

    Add tesseract to preconfigured packages and custom steps

---
+++ custom-steps/tesseract/post-build.ps1
@@ -0,0 +1,16 @@
+param (
+    [Parameter(Mandatory=$true)][string]$BuildArtifactsPath,
+    [Parameter(Mandatory=$false)][string]$PackageAndFeatures,
+    [Parameter(Mandatory=$false)][string]$LinkType,
+    [Parameter(Mandatory=$false)][string]$BuildType,
+    [Parameter(Mandatory=$false)][string]$ModulesRoot    
+)
+
+$moduleName = "Build"
+if ( -not ( Get-Module -Name $moduleName ) ) {
+    Import-Module "$ModulesRoot/$moduleName" -Force -DisableNameChecking
+}
+
+if ( Get-IsOnWindowsOS ) {
+    Update-VersionInfoForDlls -buildArtifactsPath $buildArtifactsPath -versionInfoJsonPath "$PSScriptRoot/version-info.json"
+}
