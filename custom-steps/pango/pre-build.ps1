8842c2e393b3349b18dffad19da291625fef6288
Mike Malburg <m.malburg@techsmith.com>
2024-08-23 08:46:48 -0400

    Update pre-build step for installing setuputils
    While doing `brew install setuputils` resolved the issue on my Intel Mac, it did not resolve the issue on the build server.  I am trying this again, but using pip instead, as per this answer: https://stackoverflow.com/questions/77233855/why-did-i-get-an-error-modulenotfounderror-no-module-named-distutils

--- custom-steps/pango/pre-build.ps1
+++ custom-steps/pango/pre-build.ps1
@@ -3,5 +3,5 @@ Import-Module "$PSScriptRoot/../../ps-modules/Build" -DisableNameChecking
 if (-not (Get-IsOnMacOS)) {
     exit
 }
-Write-Message "Installing python-setuptools..."
-brew install python-setuptools
+Write-Message "Installing setuptools..."
+python3 -m pip install setuptools
