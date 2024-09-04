76630ce9edfa3c292e2a4877b8c9d13097049700
Mike Malburg <m.malburg@techsmith.com>
2024-09-04 16:24:30 -0400

    Add patch to disable profiles other than AAC-LC for encoding
    This should just disable ENCODING of AAC audio for profiles other than AAC-LC, and not decoding.
    
    FF_AAC_PROFILE_OPTS is used in 2 places:
    1. libavcodec/aacenc.c: FFMPEG's default AAC encoder.  This is used to ultimately build up "ff_aac_encoder".
    2. libavcodec/libfdk-aacenc.c: We don't have this encoder enabled in our build

--- custom-ports/ffmpeg/portfile.cmake
+++ custom-ports/ffmpeg/portfile.cmake
@@ -17,6 +17,7 @@ vcpkg_from_github(
         0040-ffmpeg-add-av_stream_get_first_dts-for-chromium.patch # Do not remove this patch. It is required by chromium
         0041-add-const-for-opengl-definition.patch
         0042-fix-arm64-linux.patch #https://github.com/FFmpeg/FFmpeg/commit/fcfd17dbb4a6cf270cdd82e91c21a5efdc878d12
+        1001-tsc-disable-non-aac-lc.patch # TechSmith patch
 )
 
 if(SOURCE_PATH MATCHES " ")
