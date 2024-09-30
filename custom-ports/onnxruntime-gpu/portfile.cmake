243e6fa93a6ff7c01dab75e6384900632dbf2d92
Mike Malburg <m.malburg@techsmith.com>
2024-09-30 13:48:47 -0400

    Add Mac support for onnxruntime-gpu

--- custom-ports/onnxruntime-gpu/portfile.cmake
+++ custom-ports/onnxruntime-gpu/portfile.cmake
@@ -1,10 +1,21 @@
 vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/microsoft/onnxruntime/releases/download/v${VERSION}/onnxruntime-win-x64-gpu-${VERSION}.zip"
-    FILENAME "onnxruntime-win-x64-gpu-${VERSION}.zip"
-    SHA512 9576eafca59fc7f2af9f62d7ee8aa31208ef965d17f3ad71747d5a9a46cdffd6b3958dc945109d82937555df8bb35319ce92925e66ab707f1ca8e7564ecb3ced
-)
+set(PKG_NAME "")
+if(VCPKG_HOST_IS_WINDOWS)
+    set(PKG_NAME "onnxruntime-win-x64-gpu-${VERSION}")
+    vcpkg_download_distfile(ARCHIVE
+        URLS "https://github.com/microsoft/onnxruntime/releases/download/v${VERSION}/${PKG_NAME}.zip"
+        FILENAME "${PKG_NAME}.zip"
+        SHA512 9576eafca59fc7f2af9f62d7ee8aa31208ef965d17f3ad71747d5a9a46cdffd6b3958dc945109d82937555df8bb35319ce92925e66ab707f1ca8e7564ecb3ced
+    )
+elseif(VCPKG_HOST_IS_OSX)
+    set(PKG_NAME "onnxruntime-osx-universal2-${VERSION}")
+    vcpkg_download_distfile(ARCHIVE
+        URLS "https://github.com/microsoft/onnxruntime/releases/download/v${VERSION}/${PKG_NAME}.zip"
+        FILENAME "${PKG_NAME}.zip"
+        SHA512 a4b233f5cb258624af4fa57c10c93010c01730d067809054846a7e9eec3f8d5025a52cc07f920258b0db5073a787d8fcffa5f627c0dd34c690900c836d797d49
+    )
+endif()
 
 vcpkg_extract_source_archive(
     SOURCE_PATH
@@ -21,82 +32,41 @@ vcpkg_from_github(
 )
 
 file(COPY
-        ${REPO_PATH}/include/onnxruntime/core/session/experimental_onnxruntime_cxx_api.h 
-        ${REPO_PATH}/include/onnxruntime/core/session/experimental_onnxruntime_cxx_inline.h
-        DESTINATION ${CURRENT_PACKAGES_DIR}/include
-    )
+    ${REPO_PATH}/include/onnxruntime/core/session/experimental_onnxruntime_cxx_api.h 
+    ${REPO_PATH}/include/onnxruntime/core/session/experimental_onnxruntime_cxx_inline.h
+    DESTINATION ${CURRENT_PACKAGES_DIR}/include
+)
 
 file(MAKE_DIRECTORY
-        ${CURRENT_PACKAGES_DIR}/include
-        ${CURRENT_PACKAGES_DIR}/lib
+    ${CURRENT_PACKAGES_DIR}/include
+    ${CURRENT_PACKAGES_DIR}/lib
+)
+
+if(VCPKG_HOST_IS_WINDOWS)
+    file(MAKE_DIRECTORY
         ${CURRENT_PACKAGES_DIR}/bin
-        ${CURRENT_PACKAGES_DIR}/debug/lib
-        ${CURRENT_PACKAGES_DIR}/debug/bin
     )
+endif()
 
 file(COPY
-        ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/include
-        DESTINATION ${CURRENT_PACKAGES_DIR}
-    )
+    ${SOURCE_PATH}/${PKG_NAME}/include
+    DESTINATION ${CURRENT_PACKAGES_DIR}
+)
 
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/lib/onnxruntime.lib
-    DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/lib/onnxruntime.lib
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
+if(VCPKG_HOST_IS_OSX)
+    file(GLOB LIB_FILES "${SOURCE_PATH}/${PKG_NAME}/*.lib")
+    file(GLOB BIN_FILES "${SOURCE_PATH}/${PKG_NAME}/*.dll" "${SOURCE_PATH}/${PKG_NAME}/*.pdb")
+elseif(VCPKG_HOST_IS_WINDOWS)
+    file(GLOB LIB_FILES "${SOURCE_PATH}/${PKG_NAME}/*.dylib" "${SOURCE_PATH}/${PKG_NAME}/*.dSYM")
+endif()
 
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/lib/onnxruntime.pdb
-    DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/lib/onnxruntime.pdb
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
+foreach(file ${LIB_FILES})
+    file(COPY ${SOURCE_PATH}/${PKG_NAME}/lib/${file} DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
+endforeach()
 
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/lib/onnxruntime_providers_cuda.lib
-    DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/lib/onnxruntime_providers_cuda.lib
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
+foreach(file ${BIN_FILES})
+    file(COPY ${SOURCE_PATH}/${PKG_NAME}/lib/${file} DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
+endforeach()
 
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/lib/onnxruntime_providers_cuda.pdb
-    DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/lib/onnxruntime_providers_cuda.pdb
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
-
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/lib/onnxruntime_providers_shared.pdb
-    DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/lib/onnxruntime_providers_shared.pdb
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
-
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/lib/onnxruntime_providers_tensorrt.pdb
-    DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/lib/onnxruntime_providers_tensorrt.pdb
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
-
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/lib/onnxruntime_providers_shared.lib
-    DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/lib/onnxruntime_providers_shared.lib
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
-
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/lib/onnxruntime_providers_tensorrt.lib
-    DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/lib/onnxruntime_providers_tensorrt.lib
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
-
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/lib/onnxruntime_providers_shared.dll
-    DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/lib/onnxruntime_providers_shared.dll
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
-
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/lib/onnxruntime.dll
-    DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/lib/onnxruntime.dll
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
-
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/lib/onnxruntime_providers_tensorrt.dll
-    DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/lib/onnxruntime_providers_tensorrt.dll
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
-
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/lib/onnxruntime_providers_cuda.dll
-    DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/lib/onnxruntime_providers_cuda.dll
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
-# # Handle copyright
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/onnxruntime-win-x64-gpu-${VERSION}/LICENSE")
+# Handle copyright
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/${PKG_NAME}/LICENSE")
