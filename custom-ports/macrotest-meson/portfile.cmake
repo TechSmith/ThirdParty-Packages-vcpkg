f3b952dfed0176867cfddeb50c82822005c3f634
Mike Malburg <m.malburg@techsmith.com>
2024-11-04 10:32:31 -0500

    fixup! +macrotest-meson port

--- custom-ports/macrotest-meson/portfile.cmake
+++ custom-ports/macrotest-meson/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO mike-malburg/macrotest-meson
     REF main
-    SHA512 291f6d4a1d98f87123322fb4e60072df600bd7f7f3f344bc7f294e6ce91958ea36be98d40afecdab647b03ad1723a213db12b40d9f192041784a9d026bd9bc03
+    SHA512 54a3181f3ba1f80e211c5f1e99ef0bd72229459af2ccb1d821195f7dbe37008333ddd1674f44c16317a27eaae4c44e50ce0d36853bf3c295bfd5ec9a78eb01bb
     HEAD_REF main
 )
 
