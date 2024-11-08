da33439e51872d3d231c1f89662c79365b30b56e
David Cheng <d.cheng@techsmith.com>
2024-11-08 15:37:43 -0500

    Fix windows build

--- custom-ports/dawn/portfile.cmake
+++ custom-ports/dawn/portfile.cmake
@@ -6,39 +6,35 @@ vcpkg_from_git(
    HEAD_REF main
 )
 
-#set(VCPKG_POLICY_SKIP_MISPLACED_CMAKE_FILES_CHECK enabled)
-#set(VCPKG_POLICY_SKIP_LIB_CMAKE_MERGE_CHECK enabled)
+vcpkg_find_acquire_program(GIT)
+vcpkg_find_acquire_program(PYTHON3)
+vcpkg_execute_required_process(
+   COMMAND ${PYTHON3} ${SOURCE_PATH}/tools/fetch_dawn_dependencies.py --git ${GIT}
+   WORKING_DIRECTORY ${SOURCE_PATH}
+   LOGNAME fetch_dawn_dependencies
+)
 
-set(BUILD_DIR ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel) # Fragile! Not sure why there's a -rel suffix
+list(APPEND CONFIGURE_OPTIONS
+   -DDAWN_FETCH_DEPENDENCIES=OFF # We'll call this ourselves
+   -DDAWN_BUILD_SAMPLES=OFF
+   -DDAWN_ENABLE_VULKAN=OFF
+   -DDAWN_ENABLE_INSTALL=ON
+   -DTINT_BUILD_SPV_READER=ON
+   -DTINT_ENABLE_INSTALL=ON
+   -DTINT_BUILD_TESTS=OFF
+   -DBUILD_SHARED_LIBS=OFF
+)
 
 if(VCPKG_HOST_IS_OSX)
-   vcpkg_cmake_configure(
-      SOURCE_PATH ${SOURCE_PATH}
-      OPTIONS
-      -DDAWN_FETCH_DEPENDENCIES=ON
-      -DDAWN_BUILD_SAMPLES=OFF
-      -DDAWN_ENABLE_VULKAN=OFF
-      -DDAWN_ENABLE_INSTALL=ON
-      -DTINT_BUILD_SPV_READER=ON
-      -DTINT_ENABLE_INSTALL=ON
-      -DTINT_BUILD_TESTS=OFF
-      -DBUILD_SHARED_LIBS=OFF
+   list(APPEND CONFIGURE_OPTIONS
       -DCMAKE_OSX_ARCHITECTURES=arm64;x86_64
    )
-else()
-   vcpkg_cmake_configure(
-      SOURCE_PATH ${SOURCE_PATH}
-      OPTIONS
-      -DDAWN_FETCH_DEPENDENCIES=ON
-      -DDAWN_BUILD_SAMPLES=OFF
-      -DDAWN_ENABLE_VULKAN=OFF
-      -DDAWN_ENABLE_INSTALL=ON
-      -DTINT_BUILD_SPV_READER=ON
-      -DTINT_ENABLE_INSTALL=ON
-      -DTINT_BUILD_TESTS=OFF
-      -DBUILD_SHARED_LIBS=OFF
-   )
 endif()
+
+vcpkg_cmake_configure(
+   SOURCE_PATH ${SOURCE_PATH}
+   OPTIONS ${CONFIGURE_OPTIONS}
+)
 
 vcpkg_build_cmake()
 
@@ -46,32 +42,47 @@ vcpkg_build_cmake()
 # these files to exist after a build, but they don't.
 #
 # We'll just make some empty files so the CMake install process doesn't fail
+set(BUILD_DIR ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel) # Fragile! Not sure why there's a -rel suffix
+
 set(PREFIX ${VCPKG_TARGET_STATIC_LIBRARY_PREFIX})
 set(SUFFIX ${VCPKG_TARGET_STATIC_LIBRARY_SUFFIX})
-set(MISSING_FILES
-   ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_glsl_intrinsic${SUFFIX}
-   ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_glsl_ir${SUFFIX}
-   ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_glsl${SUFFIX}
-   ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_msl_intrinsic${SUFFIX}
-   ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_msl_ir${SUFFIX}
-   ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_msl_type${SUFFIX}
-   ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_msl${SUFFIX}
-   ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_hlsl_intrinsic${SUFFIX}
-   ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_hlsl_ir${SUFFIX}
-   ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_hlsl_type${SUFFIX}
-   ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_hlsl_writer_printer${SUFFIX}
-   ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_hlsl_writer_raise${SUFFIX}
-   ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_hlsl${SUFFIX}
-   ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_spirv_intrinsic${SUFFIX}
-   ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_spirv_ir${SUFFIX}
-   ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_spirv_type${SUFFIX}
-   ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_spirv${SUFFIX}
-   ${BUILD_DIR}/src/tint/${PREFIX}tint_utils_bytes${SUFFIX}
-)
 
-file(MAKE_DIRECTORY ${BUILD_DIR}/src/tint)
-foreach(MISSING_FILE ${MISSING_FILES})
-   file(TOUCH ${MISSING_FILE})
+if(VCPKG_BUILD_TYPE STREQUAL "Debug")
+   list(APPEND BUILD_DIR_SUFFIXES "-dbg")
+elseif(VCPKG_BUILD_TYPE STREQUAL "Release")
+   list(APPEND BUILD_DIR_SUFFIXES "-rel")
+else()
+   list(APPEND BUILD_DIR_SUFFIXES "-dbg")
+   list(APPEND BUILD_DIR_SUFFIXES "-rel")
+endif()
+
+foreach(BUILD_DIR_SUFFIX ${BUILD_DIR_SUFFIXES})
+   set(BUILD_DIR ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET} ${BUILD_DIR_SUFFIX})
+   set(MISSING_FILES
+      ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_glsl_intrinsic${SUFFIX}
+      ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_glsl_ir${SUFFIX}
+      ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_glsl${SUFFIX}
+      ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_msl_intrinsic${SUFFIX}
+      ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_msl_ir${SUFFIX}
+      ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_msl_type${SUFFIX}
+      ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_msl${SUFFIX}
+      ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_hlsl_intrinsic${SUFFIX}
+      ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_hlsl_ir${SUFFIX}
+      ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_hlsl_type${SUFFIX}
+      ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_hlsl_writer_printer${SUFFIX}
+      ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_hlsl_writer_raise${SUFFIX}
+      ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_hlsl${SUFFIX}
+      ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_spirv_intrinsic${SUFFIX}
+      ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_spirv_ir${SUFFIX}
+      ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_spirv_type${SUFFIX}
+      ${BUILD_DIR}/src/tint/${PREFIX}tint_lang_spirv${SUFFIX}
+      ${BUILD_DIR}/src/tint/${PREFIX}tint_utils_bytes${SUFFIX}
+   )
+   file(MAKE_DIRECTORY ${BUILD_DIR}/src/tint)
+   foreach(MISSING_FILE ${MISSING_FILES})
+      message("Creating empty file ${MISSING_FILE}")
+      file(TOUCH ${MISSING_FILE})
+   endforeach()
 endforeach()
 
 message("*** run CMake Install...")
