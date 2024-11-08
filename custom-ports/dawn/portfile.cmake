eab58c2f5e2d1b8187bd99bc8b2aa1e93e0c061e
David Cheng <d.cheng@techsmith.com>
2024-11-08 09:55:59 -0500

    Copy missing src/utils/compiler.h ... + cleanup

--- custom-ports/dawn/portfile.cmake
+++ custom-ports/dawn/portfile.cmake
@@ -9,9 +9,7 @@ vcpkg_from_git(
 #set(VCPKG_POLICY_SKIP_MISPLACED_CMAKE_FILES_CHECK enabled)
 #set(VCPKG_POLICY_SKIP_LIB_CMAKE_MERGE_CHECK enabled)
 
-message("hello from dawn's vcpkg portfile")
 set(BUILD_DIR ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel) # Fragile! Not sure why there's a -rel suffix
-message("*** Add some files that are missing that install expects. BUILD_DIR = ${BUILD_DIR}")
 
 if(VCPKG_HOST_IS_OSX)
    vcpkg_cmake_configure(
@@ -42,11 +40,12 @@ else()
    )
 endif()
 
-message("*** Building with cmake")
 vcpkg_build_cmake()
 
-message("*** Add some files that are missing that install expects. BUILD_DIR = ${BUILD_DIR}")
-
+# Tint's install doesn't seem to properly respond to the CMake options. It expects
+# these files to exist after a build, but they don't.
+#
+# We'll just make some empty files so the CMake install process doesn't fail
 set(PREFIX ${VCPKG_TARGET_STATIC_LIBRARY_PREFIX})
 set(SUFFIX ${VCPKG_TARGET_STATIC_LIBRARY_SUFFIX})
 set(MISSING_FILES
@@ -72,10 +71,18 @@ set(MISSING_FILES
 
 file(MAKE_DIRECTORY ${BUILD_DIR}/src/tint)
 foreach(MISSING_FILE ${MISSING_FILES})
-   message("- creating missing file ${MISSING_FILE}")
    file(TOUCH ${MISSING_FILE})
 endforeach()
 
 message("*** run CMake Install...")
 vcpkg_install_cmake()
+
+set(DST_DIR "${CURRENT_PACKAGES_DIR}/include/src/utils")
+message("Copying from ${SOURCE_PATH}/src/utils/compiler.h to ${DST_DIR}")
+if(NOT EXISTS ${DST_DIR})
+   file(MAKE_DIRECTORY ${DST_DIR})
+endif()
+file(COPY "${SOURCE_PATH}/src/utils/compiler.h"
+   DESTINATION ${DST_DIR})
+
 vcpkg_copy_pdbs()
