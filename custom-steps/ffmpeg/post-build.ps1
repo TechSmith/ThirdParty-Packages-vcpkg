eb8d423ce93871c9be3da9b6f31e754e43cfea3b
Mike Malburg <m.malburg@techsmith.com>
2024-11-19 19:53:35 -0500

    Update ffmpeg exe to point to the appropriate .dylib files

--- custom-steps/ffmpeg/post-build.ps1
+++ custom-steps/ffmpeg/post-build.ps1
@@ -15,3 +15,18 @@ if((Get-IsOnWindowsOS)) {
     Update-VersionInfoForDlls -buildArtifactsPath $buildArtifactsPath -versionInfoJsonPath "$PSScriptRoot/version-info.json"
 }
 
+if((Get-IsOnMacOS)) {
+    Write-Host "Updating library paths in ffmpeg executable..."
+    $binaryPath = "$buildArtifactsPath/tools/ffmpeg/ffmpeg"
+    $otoolOutput = & "otool" "-L" $binaryPath
+    foreach ($line in $otoolOutput) {
+        if ($line -match '@rpath\/([^\.]+)\.[^\/]*\.dylib') {
+            $originalPath = $matches[0]
+            $newPath = "@rpath/$($matches[1]).dylib"
+    
+            Write-Output ">> Updating $originalPath to $newPath"
+            & "install_name_tool" "-change" $originalPath $newPath $binaryPath
+        }
+    }
+}
+
