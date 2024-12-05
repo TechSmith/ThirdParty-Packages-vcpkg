48e6b83667b4e99af78aed0c35279f04b2c20398
Mike Malburg <m.malburg@techsmith.com>
2024-12-05 12:51:28 -0500

    Add post-build tests

--- custom-steps/ffmpeg/post-build.ps1
+++ custom-steps/ffmpeg/post-build.ps1
@@ -6,17 +6,23 @@ param (
     [Parameter(Mandatory=$false)][string]$ModulesRoot    
 )
 
-$moduleName = "Build"
-if(-not (Get-Module -Name $moduleName)) {
-    Import-Module "$ModulesRoot/$moduleName" -Force -DisableNameChecking
+# Import modules
+$moduleNames = @("Build", "Util")
+foreach( $moduleName in $moduleNames ) {
+    if(-not (Get-Module -Name $moduleName)) {
+        Import-Module "$ModulesRoot/$moduleName" -Force -DisableNameChecking
+    }
 }
 
+$pathToTools = "$BuildArtifactsPath/tools/ffmpeg"
+$pathToFFmpegExe = ""
 if((Get-IsOnWindowsOS)) {
     Update-VersionInfoForDlls -buildArtifactsPath $buildArtifactsPath -versionInfoJsonPath "$PSScriptRoot/version-info.json"
+    $pathToFFmpegExe = "$pathToTools/ffmpeg.exe"
 }
 
 if((Get-IsOnMacOS)) {
-    Write-Host "Updating library paths in ffmpeg executable..."
+    Write-Message "> Updating library paths in ffmpeg executable..."
     $binaryPath = "$buildArtifactsPath/tools/ffmpeg/ffmpeg"
     $otoolOutput = & "otool" "-L" $binaryPath
     foreach ($line in $otoolOutput) {
@@ -28,5 +34,34 @@ if((Get-IsOnMacOS)) {
             & "install_name_tool" "-change" $originalPath $newPath $binaryPath
         }
     }
+    $pathToFFmpegExe = "$pathToTools/ffmpeg"
 }
 
+Write-Message "$(NL)Running post-build tests..."
+$finalExitCode = 0
+$testScriptArgs = @{ 
+  BuildArtifactsPath = $BuildArtifactsPath
+  FFMpegExePath = $pathToFFmpegExe
+  OutputDir = "test-output"
+}
+Push-Location $PSScriptRoot
+if (Test-Path $testScriptArgs.OutputDir) {
+  Remove-Item -Path $testScriptArgs.OutputDir -Recurse -Force
+}
+Invoke-Powershell -FilePath "test001--query-capabilities.ps1" -ArgumentList $testScriptArgs
+$scriptReturnCode = $LASTEXITCODE
+Write-Host "$(NL)> Script exit code = $scriptReturnCode"
+if ( ($finalExitCode -eq 0) -and ($returnCode -ne 0) ) {
+    $finalExitCode = $scriptReturnCode
+}
+
+Invoke-Powershell -FilePath "test002--verify-encoding.ps1" -ArgumentList $testScriptArgs
+$scriptReturnCode = $LASTEXITCODE
+Write-Host "$(NL)> Script exit code = $scriptReturnCode"
+if ( ($finalExitCode -eq 0) -and ($returnCode -ne 0) ) {
+    $finalExitCode = $scriptReturnCode
+}
+
+Pop-Location
+
+Exit $finalExitCode
