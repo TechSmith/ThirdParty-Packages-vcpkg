7f31f2b1bad274874ab2e09f7812868575ef0592
David Cheng <d.cheng@techsmith.com>
2024-12-17 11:34:10 -0500

    Can cross-compile to wasm now
    TargetOS is specifies which entry in preconfigured-packages.json should be used
    
    Also tweaked the logic behind whether we are building universal binaries or not

--- build-package.ps1
+++ build-package.ps1
@@ -1,17 +1,22 @@
 param(
     [Parameter(Mandatory=$true)][string]$PackageName, # Name of package from preconfigured-packages.json
     [string]$StagedArtifactsPath = "StagedArtifacts", # Output path to stage these artifacts to
-    [switch]$ShowDebug = $false
+    [switch]$ShowDebug = $false,
+    [string]$TargetOS = $null
 )
 
 Import-Module "$PSScriptRoot/scripts/ps-modules/Build" -Force -DisableNameChecking
- 
+
 Write-Banner -Level 1 -Title "Installing preconfigured package: `"$PackageName`""
-$pkg = Get-PackageInfo -PackageName $PackageName
+
+if($TargetOS -eq $null) {
+    $TargetOS = Get-OSType
+}
+
+$pkg = Get-PackageInfo -PackageName $PackageName -TargetOS $TargetOS
 
 if ($pkg -eq $null) {
-    $selectedSection = Get-OSType
-    Write-Message "Package $PackageName contains no section for $selectedSection, skipping build."
+    Write-Message "Package $PackageName contains no section for TargetOS = $TargetOS, skipping build."
     exit 0
 }
 
