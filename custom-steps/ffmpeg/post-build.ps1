a320fb86a77979866baba6829024d5c5ffcef686
Mike Malburg <m.malburg@techsmith.com>
2025-01-10 15:42:08 -0500

    +ffmpeg post-build tests

--- custom-steps/ffmpeg/post-build.ps1
+++ custom-steps/ffmpeg/post-build.ps1
@@ -11,7 +11,43 @@ if(-not (Get-Module -Name $moduleName)) {
     Import-Module "$ModulesRoot/$moduleName" -Force -DisableNameChecking
 }
 
+if( Check-IsEmscriptenBuild -triplets $Triplets ) {
+   Exit 0
+}
+
+$pathToTools = "$BuildArtifactsPath/tools/ffmpeg"
+$pathToFFmpegExe = ""
 if((Get-IsOnWindowsOS)) {
     Update-VersionInfoForDlls -buildArtifactsPath $buildArtifactsPath -versionInfoJsonPath "$PSScriptRoot/version-info.json"
 }
 
+
+Write-Message "$(NL)Running post-build tests..."
+$finalExitCode = 0
+$testScriptArgs = @{ 
+  BuildArtifactsPath = $BuildArtifactsPath
+  ModulesRoot = $ModulesRoot
+  FFMpegExePath = $pathToFFmpegExe
+  OutputDir = "test-output"
+}
+Push-Location $PSScriptRoot
+if (Test-Path $testScriptArgs.OutputDir) {
+  Remove-Item -Path $testScriptArgs.OutputDir -Recurse -Force
+}
+Invoke-Powershell -FilePath "test001--query-capabilities.ps1" -ArgumentList $testScriptArgs
+$scriptReturnCode = $LASTEXITCODE
+Write-Host "$(NL)> Script exit code = $scriptReturnCode"
+if ( ($finalExitCode -eq 0) -and ($returnCode -ne 0) ) {
+    $finalExitCode = $scriptReturnCode
+}
+
+Invoke-Powershell -FilePath "test002--verify-encoding.ps1" -ArgumentList $testScriptArgs
+$scriptReturnCode = $LASTEXITCODE
+Write-Host "$(NL)> Script exit code = $scriptReturnCode"
+if ( ($finalExitCode -eq 0) -and ($returnCode -ne 0) ) {
+    $finalExitCode = $scriptReturnCode
+}
+
+Pop-Location
+
+Exit $finalExitCode
