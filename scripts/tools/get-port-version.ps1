60ed33adfba9fa77cf2dc535b1b4ed91fc765522
Mike Malburg <m.malburg@techsmith.com>
2025-06-30 13:51:25 -0400

    fixup! +scripts/tools/get-port-version.ps1

--- scripts/tools/get-port-version.ps1
+++ scripts/tools/get-port-version.ps1
@@ -6,10 +6,13 @@ param(
 
 Import-Module "$PSScriptRoot/../ps-modules/Build" -Force -DisableNameChecking
 
+Write-Host "> PackageName is: $PackageName"
 $myOverlayPath = Join-Path $PSScriptRoot "../../$OverlayPortsPath"
 $pkg = Get-PackageInfo -PackageName $PackageName -TargetPlatform $TargetPlatform
 $portName = (Get-PortNameOnly $pkg.package)
-$portVersion = Get-VcpkgPortVersion -portName $PortName -overlayPortsPath $myOverlayPath
+Write-Host "> PortName is: $portName"
+
+$portVersion = Get-VcpkgPortVersion -portName $portName -overlayPortsPath $myOverlayPath
 if ($portVersion) {
     Write-Host $portVersion
 }
