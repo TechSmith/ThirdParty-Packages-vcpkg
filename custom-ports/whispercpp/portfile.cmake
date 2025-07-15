6532e6cfa946d60284dd880f792fccd11c994f90
Mike Malburg <m.malburg@techsmith.com>
2025-07-15 18:16:08 -0400

    Combine whisper ports into one

--- custom-ports/whispercpp/portfile.cmake
+++ custom-ports/whispercpp/portfile.cmake
@@ -1,44 +1,59 @@
-# whispercpp
+if("cpu-acceleration" IN_LIST FEATURES)
+    SET(FEATURE_CPU_ACCELERATION ON)
+endif()
+
+if(FEATURE_CPU_ACCELERATION)
+    list(APPEND FEATURE_PATCH_LIST "0001-UpdateTargetName-whisper-cpuaccel.patch")
+endif()
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ggerganov/whisper.cpp
     REF "v${VERSION}"
     SHA512 7e0ec9d6afe234afaaa83d7d69051504252c27ecdacbedf3d70992429801bcd1078794a0bb76cf4dafb74131dd0f506bd24c3f3100815c35b8ac2b12336492ef
     HEAD_REF master
+    PATCHES
+        ${FEATURE_PATCH_LIST}
 )
 
 set(VCPKG_POLICY_SKIP_MISPLACED_CMAKE_FILES_CHECK enabled)
-set(VCPKG_POLICY_SKIP_LIB_CMAKE_MERGE_CHECK enabled)
 
 # Make git path available for the whispercpp project cmake files
 vcpkg_find_acquire_program(GIT)
 get_filename_component(GIT_DIR "${GIT}" DIRECTORY)
 vcpkg_add_to_path("${GIT_DIR}")
 
+# CMake configure
+set(CONFIG_OPTIONS
+    -DWHISPER_BUILD_EXAMPLES=OFF
+    -DWHISPER_BUILD_TESTS=OFF
+    -DWHISPER_BUILD_SERVER=OFF
+)
 if(VCPKG_HOST_IS_OSX)
-    vcpkg_cmake_configure(
-        SOURCE_PATH ${SOURCE_PATH}
-        OPTIONS
-            -DGGML_METAL_EMBED_LIBRARY=ON
-            -DGGML_METAL_NDEBUG=ON
-    )
-else()
-    vcpkg_cmake_configure(
-        SOURCE_PATH ${SOURCE_PATH}
+    list(APPEND CONFIG_OPTIONS
+        -DGGML_METAL_EMBED_LIBRARY=ON
+        -DGGML_METAL_NDEBUG=ON
+        -DGGML_METAL=${FEATURE_CPU_ACCELERATION}
     )
 endif()
+list(APPEND CONFIG_OPTIONS
+    -DGGML_AVX=${FEATURE_CPU_ACCELERATION}
+    -DGGML_AVX2=${FEATURE_CPU_ACCELERATION}
+    -DGGML_FMA=${FEATURE_CPU_ACCELERATION}
+    -DGGML_F16C=${FEATURE_CPU_ACCELERATION}
+)
+vcpkg_cmake_configure(
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        ${CONFIG_OPTIONS}
+)
 
-vcpkg_install_cmake()
-vcpkg_copy_pdbs()
+# CMake Build & install
+vcpkg_cmake_install()
 
-# Store all .libs in the root lib directory.  For Windows SHARED builds, these will be import libraries (not static libs).
-file(GLOB_RECURSE LIB_FILES "${CURRENT_PACKAGES_DIR}/lib/static/*.lib")
-foreach(LIB_FILE ${LIB_FILES})
-    get_filename_component(LIB_NAME ${LIB_FILE} NAME)
-    file(RENAME ${LIB_FILE} "${CURRENT_PACKAGES_DIR}/lib/${LIB_NAME}")
-endforeach()
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/static")
-
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-
+# Other packaging steps
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/whisper)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/ggml)
 vcpkg_fixup_pkgconfig()
+vcpkg_copy_pdbs()
+vcpkg_install_copyright(FILE_LIST ${SOURCE_PATH}/LICENSE)
