9e08647c1d3a4f99704702bb802d5d7b1af2c9f7
Dmytro Kiro <d.kiro@techsmith.com>
2026-01-20 12:25:01 -0500

    Add -fPIC flag to wasm32-emscripten-static triplet for MAIN_MODULE linking

--- custom-triplets/wasm32-emscripten-static.cmake
+++ custom-triplets/wasm32-emscripten-static.cmake
@@ -23,3 +23,7 @@ set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Emscripten)
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${EMSCRIPTEN_ROOT}/cmake/Modules/Platform/Emscripten.cmake")
+
+# Add -fPIC flag for position-independent code (required for MAIN_MODULE linking)
+set(VCPKG_C_FLAGS "-fPIC")
+set(VCPKG_CXX_FLAGS "-fPIC")
