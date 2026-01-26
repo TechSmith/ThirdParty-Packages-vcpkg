f42dfebb4c2ac94b8f6dfeef4aaa8b49f539545d
Mike Malburg <m.malburg@techsmith.com>
2026-01-26 10:12:01 -0500

    Enable AVX2 auto-vectorization for Windows dynamic builds

--- custom-triplets/x64-windows-dynamic-debug.cmake
+++ custom-triplets/x64-windows-dynamic-debug.cmake
@@ -2,3 +2,6 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 set(VCPKG_BUILD_TYPE debug)
+
+set(VCPKG_CXX_FLAGS "/arch:AVX2")
+set(VCPKG_C_FLAGS "/arch:AVX2")
