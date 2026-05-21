5c1e7ac9904fd4b24e9ca5d049e1338f0a642193
Mike Malburg <m.malburg@techsmith.com>
2026-05-21 13:06:11 -0400

    Remove VCPKG_BUILD_TYPE from Mac static-debug triplets to fix abseil pkgconfig

--- custom-triplets/arm64-osx-static-debug.cmake
+++ custom-triplets/arm64-osx-static-debug.cmake
@@ -5,7 +5,9 @@ set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES arm64)
 
-set(VCPKG_BUILD_TYPE debug)
+# VCPKG_BUILD_TYPE intentionally omitted - builds both debug+release
+# to avoid abseil pkgconfig fixup failures with debug-only builds.
+# Only debug artifacts are published (controlled by preconfigured-packages.json).
 
 set(VCPKG_C_FLAGS "-mmacosx-version-min=11.0 -g -gdwarf-2")
 set(VCPKG_CXX_FLAGS "-mmacosx-version-min=11.0 -g -gdwarf-2")
