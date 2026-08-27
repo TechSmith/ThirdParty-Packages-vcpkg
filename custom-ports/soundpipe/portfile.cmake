a555babaf207578e460f62b3ef31c7242ff0f4d0
Dmytro Kiro <d.kiro@techsmith.com>
2026-08-27 14:51:15 -0700

    main instead of master

--- custom-ports/soundpipe/portfile.cmake
+++ custom-ports/soundpipe/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_from_github(
     REPO TechSmith/Soundpipe
     REF 3efb43bdabd0ed23b17c694292b5a79f1692a3ea
     SHA512 6a20b5fc7420a062a79bd22d2b0aa731acb7a0ac0ee587cfedd36918d7344848335b2da159bf630d869817c47986105522bcfbb3d6c961c31ef51b8f2db9d6a5
-    HEAD_REF master
+    HEAD_REF main
     PATCHES
         include-spa-header.patch
 )
